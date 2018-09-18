91ecae027d7327d94532158e33f8fe896bb3747c
danielthestranger <danielthestranger@gmail.com>
2018-09-18 15:31:47 +0200

    feat(BankAccountController): implement /show-all GET endpoint

--- src/main/java/com/greenfoxacademy/bankofsimba/controllers/BankAccountController.java
+++ src/main/java/com/greenfoxacademy/bankofsimba/controllers/BankAccountController.java
@@ -5,8 +5,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.lang.reflect.Array;
 import java.math.BigDecimal;
+import java.util.Arrays;
 import java.util.Currency;
+import java.util.List;
 
 @Controller
 public class BankAccountController {
@@ -21,4 +24,33 @@ public class BankAccountController {
         model.addAttribute("bankAccount", bankAccount);
         return "bank-account";
     }
+
+    @GetMapping("/show-list")
+    public String showBankAccounts(Model model) {
+        BankAccount simba = new BankAccount(
+                "Simba",
+                BigDecimal.valueOf(2000.),
+                "Zebra",
+                "lion");
+        BankAccount nala = new BankAccount(
+                "Nala",
+                BigDecimal.valueOf(500.),
+                "Antelope",
+                "lion");
+        BankAccount mufasa = new BankAccount(
+                "Mufasa",
+                BigDecimal.valueOf(5000.),
+                "Alopex",
+                "lion");
+        BankAccount scar = new BankAccount(
+                "Scar",
+                BigDecimal.valueOf(20.),
+                "Rat",
+                "vakarcs");
+
+        List<BankAccount> bankAccounts = Arrays.asList(simba, nala, mufasa, scar);
+        model.addAttribute("bankAccounts", bankAccounts);
+        return "bank-accounts";
+    }
+
 }
